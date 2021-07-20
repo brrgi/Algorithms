09e1833ed0073f003ba09ec4847f07b8c139a210
brrgi <48500985+brrgi@users.noreply.github.com>
2021-07-20 23:42:51 +0900

    20210720_re

--- 2_week/이레/1799_비숍.py
+++ 2_week/이레/1799_비숍.py
@@ -1,6 +1,36 @@
-n=int(input().split())
+n=int(input())
 chess=[]
+result1=[]
+result2=[]
+left=[0 for _ in range(20)]
+right=[0 for _ in range(20)]
 
 for i in range(n):
     chess.append(list(map(int,input().split())))
-print(chess)
+# print(chess)
+
+def dfs(row, col, cnt, color):
+    if col>=n:
+        row+=1
+        if col%2==0:
+            col=1
+        else:
+            col=0
+
+    if row>=n:
+        if color==1:
+            result1.append(cnt)
+        else:
+            result2.append(cnt)
+        return
+
+    if chess[row][col]==1 and left[col - row + n - 1]==0 and right[row + col]==0:
+        left[col - row + n - 1] = right[row + col] = 1
+        dfs(row, col + 2, cnt + 1, color)
+        left[col - row + n - 1] = right[row + col] = 0
+    dfs(row, col+2, cnt, color)
+
+dfs(0,0,0,0)
+dfs(0,1,0,1)
+
+print(max(result1)+max(result2))

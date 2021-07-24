b4c9b0245488fab30f97542aa1535f2d45ab0da7
brrgi <48500985+brrgi@users.noreply.github.com>
2021-07-24 18:54:36 +0900

    20210724

---
+++ 3_week/이레/15486_퇴사 2.py
@@ -0,0 +1,24 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+t=[]
+p=[]
+dp=[0 for _ in range(n+1)]
+
+
+def init(day):
+    for i in range(day):
+        ipt = list(map(int, input().split()))
+        t.append(ipt[0])
+        p.append(ipt[1])
+
+init(n)
+start=0
+
+for i in range(n):
+    start = max(start, dp[i])
+    if i+t[i]>n:
+        continue
+    dp[i+t[i]] = max(dp[i+t[i]], start+p[i])
+
+print(max(dp))

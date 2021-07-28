c37c2cbc71f3b4f65225d16045b08aa10f894970
brrgi <48500985+brrgi@users.noreply.github.com>
2021-07-28 18:10:39 +0900

    20210728_re

--- 3_week/이레/1194_달이 차오른다, 가자..py
+++ 3_week/이레/1194_달이 차오른다, 가자..py
@@ -10,14 +10,16 @@ visit = [[[0 for _ in range(64)] for _ in range(m)] for _ in range(n)]
 result = -1
 queue = deque()
 
-for i in range(n):
-    t = input().rstrip()
-    maps.append(t)
 
-for i in range(n):
-    for j in range(m):
-        if maps[i][j] == '0':
-            queue.append((i, j, 0))  # (row, col, visit)
+def init_map():
+    for i in range(n):
+        t = input().rstrip()
+        maps.append(t)
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] == '0':
+                queue.append((i, j, 0))  # (row, col, visit)
+init_map()
 
 while queue:
     data = queue.popleft()
@@ -26,7 +28,7 @@ while queue:
     z = data[2]
 
     if maps[row][col] == '1':
-        result = visit[row][col][z]
+        result = visit[row][col][z]     #출구 중 아무거나 도착하면 끝
         break
 
     for d in dir:
@@ -37,11 +39,11 @@ while queue:
         if 0 <= new_row < n and 0 <= new_col < m:
             cur = maps[new_row][new_col]
             if 97 <= ord(cur) < 123:  # 소문자 key
-                zz |= (1 << (ord(cur) - 97))
+                zz |= (1 << (ord(cur) - 97))                        #열쇠를 표현하는 bit masking
             elif 65 <= ord(cur) < 91:  # 대문자 문
-                if not zz & (1 << (ord(cur) - 65)):
+                if not zz & (1 << (ord(cur) - 65)):                 #대문자 문을 만났을 때, 열쇠가 없으면
                     continue
-            if not visit[new_row][new_col][zz] and cur != '#':
+            if not visit[new_row][new_col][zz] and cur != '#':      #zz열쇠의 상태일 때 방문 x, 그리고 벽이 아닐 때
                 queue.append((new_row, new_col, zz))
                 visit[new_row][new_col][zz] = visit[row][col][z] + 1
 

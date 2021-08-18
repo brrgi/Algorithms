cc5edb4dbc45e3518171440a31f4c22f37cc86e1
brrgi <48500985+brrgi@users.noreply.github.com>
2021-08-18 18:56:05 +0900

    20210818_re

--- 6_week/이레/16639_괄호 추가하기3.py
+++ 6_week/이레/16639_괄호 추가하기3.py
@@ -1,9 +1,3 @@
-import sys
-
-n = int(input())
-strs = input().rstrip()
-leng = n // 2 + 1
-op = []
 """
     n<19 이므로 숫자는 최대 10개
     dp[2][i][j] j->i->2(큰 값 작은 값) 은 mix min 비교가 안되므로 따로 나눌 것
@@ -11,28 +5,38 @@ op = []
     -> min_dp[i][j]
     max = sys.maxsize,  min = -sys.maxsize - 1.
 """
-max_dp = [[-sys.maxsize - 1 for _ in range(10)] for _ in range(10)]
-min_dp = [[sys.maxsize for _ in range(10)] for _ in range(10)]
+import sys
 
-for i in range(leng):
-    max_dp[i][i] = min_dp[i][i] = int(strs[2 * i])
-for i in range(leng - 1):
-    op.append(strs[2 * i + 1])
+n = int(input())
+strs = input().rstrip()
+leng = n // 2 + 1
+max_dp = [[-sys.maxsize - 1 for _ in range(n)] for _ in range(n)]
+min_dp = [[sys.maxsize for _ in range(n)] for _ in range(n)]
 
-def cal(a,b,operation):
-    if operation=='+':
-        return a+b
-    elif operation=='-':
-        return a-b
-    elif operation=='*':
-        return a*b
+for i in range(n):
+    if i%2==0:
+        max_dp[i][i] = min_dp[i][i] = int(strs[i])
 
-for q in range(leng - 1):  # ex leng = 0 1 2 3
-    for i in range(leng - (q + 1)):
-        print("구간", i, "~", q + 1 + i)
-        val=[]
-        val.append(cal(min_dp[i][i], min_dp[i+1][q+i+1], op[i]))
-        val.append(cal(max_dp[i][i], min_dp[i+1][q+i+1], op[i]))
-        val.append(cal(min_dp[i][i], min_dp[i+1][q+i+1], op[i]))
-        val.append(cal(man_dp[i][i], min_dp[i+1][q+i+1], op[i]))
-    print()
+def cal(a, b, operation):
+    if operation == '+':
+        return a + b
+    elif operation == '-':
+        return a - b
+    elif operation == '*':
+        return a * b
+
+#https://ltk3934.tistory.com/24
+for j in range(2, n, 2):
+    for i in range(0, n - j, 2):
+        for k in range(2, j + 1, 2):
+            val = []
+            val.append(cal(max_dp[i][i + k - 2], max_dp[i + k][i + j], strs[i + k - 1]))
+            val.append(cal(max_dp[i][i + k - 2], min_dp[i + k][i + j], strs[i + k - 1]))
+            val.append(cal(min_dp[i][i + k - 2], max_dp[i + k][i + j], strs[i + k - 1]))
+            val.append(cal(min_dp[i][i + k - 2], min_dp[i + k][i + j], strs[i + k - 1]))
+            val.sort()
+
+            min_dp[i][i + j] = min(min_dp[i][i + j], val[0])
+            max_dp[i][i + j] = max(max_dp[i][i + j], val[3])
+
+print(max_dp[0][n - 1])

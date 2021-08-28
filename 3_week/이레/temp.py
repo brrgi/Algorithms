e0dc86bef8b8dfeb511a0c8db846564938e7304f
brrgi <48500985+brrgi@users.noreply.github.com>
2021-08-29 00:34:22 +0900

    20210829_re

--- 3_week/이레/temp.py
+++ 3_week/이레/temp.py
@@ -1,4 +1,22 @@
-dir=[(1,0),(-1,0),(0,1),(0,-1)]
+import heapq
+import sys
 
-for x,y in dir:
-    print(x,y)
+v,e=map(int, input().split())
+k=int(input())  #시작위치
+INF=sys.maxsize
+distance=[[] for _ in range(v+1)]
+
+for _ in range(e):
+    start, end, dist=map(int, input().split())
+    distance[start].append([end, dist])
+
+queue=[]
+K_distance=[INF for _ in range(v+1)]
+K_distance[k]=0
+heapq.heappush(queue, [0,k])    #거리랑 정점
+while queue:
+    mid=heapq.heappop(queue)
+    for end in distance[mid[1]]:
+        if K_distance[end[0]]>mid[0]+end[1]:
+            K_distance[end[0]]=mid[0]+end[1]
+            heapq.heappush(queue, [K_distance[end[0]], end[0]])
---
+++ 8_week/이레/13424_비밀 모임.py
@@ -0,0 +1,30 @@
+import heapq
+import sys
+t = int(input())
+INF=sys.maxsize
+for i in range(t):
+    n, m = map(int, input().split())
+    result=[0 for _ in range(n)]
+    distance = [[] for _ in range(n + 1)]  # 정점갯수 + 1
+    secrets = []
+    for i in range(m):
+        start, end, dist=map(int, input().split())
+        distance[start].append([end, dist])
+        distance[end].append([start, dist])
+    input()
+    friends = list(map(int, input().split()))
+    for k in friends:
+        queue=[]
+        k_distance=[INF for _ in range(n+1)]
+        k_distance[k]=0
+        heapq.heappush(queue, [0,k])
+        while queue:
+            mid=heapq.heappop(queue)
+            for end in distance[mid[1]]:    #mid[1]은 현재 위치
+                if k_distance[end[0]]>mid[0]+end[1]:
+                    k_distance[end[0]]=mid[0]+end[1]
+                    heapq.heappush(queue, [k_distance[end[0]], end[0]])
+
+        for i in range(n):
+            result[i]+=k_distance[i+1]
+    print(result.index(min(result))+1)

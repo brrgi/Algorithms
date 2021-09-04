6587002a979b83bf80e4df604dbe915015d58f90
brrgi <48500985+brrgi@users.noreply.github.com>
2021-09-05 02:29:57 +0900

    20210905_re

---
+++ 9_week/이레/12764_싸지방에 간 준하.py
@@ -0,0 +1,39 @@
+import heapq
+import sys
+
+number=1
+n=int(input())
+result=0
+results=[0 for i in range(100000)]
+start=sys.maxsize
+end=-1
+queue=[]
+
+chair=[i+1 for i in range(100000)]
+heapq.heapify(chair)
+
+humans=[]
+for i in range(n):
+    t=list(map(int, input().split()))
+    start=min(start, t[0])
+    end=max(end, t[1])
+    humans.append(t)
+humans.sort(key=lambda x: x[0])
+
+ind=0
+for i in range(start, end+1):
+    if ind<n and i==humans[ind][0]:
+        temp=heapq.heappop(chair)
+        results[temp]+=1
+        heapq.heappush(queue, (humans[ind][1], temp))   #우선순위, 값
+        ind+=1
+        continue
+
+    if i==queue[0][0]:
+        temp=heapq.heappop(queue)
+        heapq.heappush(chair, temp[1])
+        continue
+
+print(len(results)-results.count(0))
+for i in range(len(results)-results.count(0)):
+    print(results[i+1], end=" ")

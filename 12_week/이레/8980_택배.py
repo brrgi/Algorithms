b8b24c104fab6600ce0c53b64a2d64a6a6240c7d
brrgi <48500985+brrgi@users.noreply.github.com>
2021-10-11 20:07:18 +0900

    20211011_re

--- 12_week/이레/8980_택배.py
+++ 12_week/이레/8980_택배.py
@@ -1,5 +1,22 @@
-import heapq
+import sys
+input=sys.stdin.readline
 n,c=map(int, input().split())   #마을 수 n, 트럭의 용량 c
 m=int(input())
+box = [0] * (n + 1)
+boxes=[]
+result=0
 for i in range(m):
     start, end, cost=map(int, input().split())
+    boxes.append([start,end,cost])
+# print(boxes)
+boxes.sort(key=lambda x:x[1])
+# print(boxes)
+
+for s, d, co in boxes:
+    max_val = co
+    for i in range(s, d):
+        max_val = min(max_val, c - box[i])
+    for i in range(s, d):
+        box[i] += max_val
+    result += max_val
+print(result)
